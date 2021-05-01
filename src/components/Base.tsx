f12f016dbafc9ef2481f0a08abb6d18f7a54db7e
Sanskaar Patni <patnisanskaar@gmail.com>
2021-05-01 21:32:55 +0530

    added hover animations

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -14,16 +14,19 @@ const Base:React.FC<Props> = ({ addBase, pizza }) => {
     initial={{ opacity: 0, x: '100vw' }}
     animate={{ opacity: 1, x: 0 }}
     transition={{ type: 'spring', delay: 0.5 }}
-  >
+    >
 
       <h3>Step 1: Choose Your Base</h3>
       <ul>
         {bases.map(base => {
           let spanClass = pizza.base === base ? 'active' : '';
           return (
-            <li key={base} onClick={() => addBase(base)}>
+            <motion.li key={base} onClick={() => addBase(base)}
+            whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
+            transition={{ type: 'spring', stiffness: 300 }}
+            >
               <span className={spanClass}>{ base }</span>
-            </li>
+            </motion.li>
           )
         })}
       </ul>
@@ -35,13 +38,20 @@ const Base:React.FC<Props> = ({ addBase, pizza }) => {
         transition={{ type: 'spring', stiffness: 120 }}
       >
         <Link to="/toppings">
-          <button>Next</button>
+        <motion.button 
+          whileHover={{
+          scale:1.1,
+          textShadow: "0px 0px 8px rgb(255,255,255)",
+          boxShadow: "0px 0px 8px rgb(255,255,255)",
+        }}
+        >
+        Next
+        </motion.button>
         </Link>
       </motion.div>
       )}
     </motion.div>
-  )
+  );
 }
-
 export default Base;
 
--- src/components/Toppings.tsx
+++ src/components/Toppings.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { pizza } from '../App';
-
+import {motion} from 'framer-motion'
 type Props={
     addTopping:(topping: string)=>void,
     pizza:pizza
@@ -17,17 +17,26 @@ const Toppings = ({ addTopping, pizza }:Props) => {
         {toppings.map(topping => {
           let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
           return (
-            <li key={topping} onClick={() => addTopping(topping)}>
+            <motion.li key={topping} onClick={() => addTopping(topping)}
+            whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
+            transition={{ type: 'spring', stiffness: 300 }}
+            >
               <span className={spanClass}>{ topping }</span>
-            </li>
+            </motion.li>
           )
         })}
       </ul>
 
       <Link to="/order">
-        <button>
-          Order
-        </button>
+      <motion.button 
+          whileHover={{
+          scale:1.1,
+          textShadow: "0px 0px 8px rgb(255,255,255)",
+          boxShadow: "0px 0px 8px rgb(255,255,255)",
+        }}
+        >
+        Order
+        </motion.button>
       </Link>
 
     </div>
